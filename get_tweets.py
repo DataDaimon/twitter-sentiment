f84b412032a13c5f2ddc75e0b4f2e760657ce791
Rick Flores <rmflores6@miners.utep.edu>
2022-12-02 21:50:38 +0900

    12-2

---
+++ get_tweets.py
@@ -0,0 +1,36 @@
+import tweepy
+import time
+import pandas as pd
+pd.set_option('display.max_colwidth', 1000)
+
+# api key
+api_key = "xxxx"
+# api secret key
+api_secret_key = "xxxx"
+# access token
+access_token = "xxxx"
+# access token secret
+access_token_secret = "xxxx"
+
+authentication = tweepy.OAuthHandler(api_key, api_secret_key)
+authentication.set_access_token(access_token, access_token_secret)
+api = tweepy.API(authentication, wait_on_rate_limit=True)
+
+def get_related_tweets(text_query):
+    # list to store tweets
+    tweets_list = []
+    # no of tweets
+    count = 50
+    try:
+        # Pulling individual tweets from query
+        for tweet in api.search_tweets(q=text_query, count=count):
+            print(tweet.text)
+            # Adding to list that contains all tweets
+            tweets_list.append({'created_at': tweet.created_at,
+                                'tweet_id': tweet.id,
+                                'tweet_text': tweet.text})
+        return pd.DataFrame.from_dict(tweets_list)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
